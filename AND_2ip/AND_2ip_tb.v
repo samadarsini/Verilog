0271133ff4703df62960409895dd5f70ba3765ac
Vemulapalli Sri Samadarsini <167125558+samadarsini@users.noreply.github.com>
2025-04-08 19:42:03 -0400

    Create AND_2ip_tb.v

---
+++ AND_2ip/AND_2ip_tb.v
@@ -0,0 +1,20 @@
+module AND_2ip_tb;
+  reg a,b;
+  wire c;
+  
+  AND_2ip and1(.c(c),.a(a),.b(b));
+  initial begin
+    #2 a=0; b=0;
+    
+    $monitor("At time %g: a=%b, b=%b, c=%b", $time, a, b, c);
+    
+    #5 a=1'b1;
+    #5 b=1'b1;
+    #5 a=1'b0;
+    #5 b=1'b0;
+    #5 b=1'b1;
+    #5 a=1'b1;
+
+    #10 $finish;
+  end
+endmodule
